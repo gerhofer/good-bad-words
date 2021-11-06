bb0f0dfa7d365bbe56bf94db3d1355297633afac
Pia Gerhofer <pia.gerhofer@teamecho.at>
2021-11-06 14:32:28 +0100

    Implement good bad words

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,27 @@
-import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import Home from './Home';
+import Result from './Result';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import { useState } from 'react';
 
 function App() {
+
+  let [word, setWord] = useState<string>()
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+      <header>
+        <img src="logo.png" alt="A speech bubble with a heart inside, next to it are the words 'Good Bad Words'"></img>
       </header>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/result" element={<Result word={word}></Result>}></Route>
+          <Route path="/" element={<Home checkWord={(wordFromHome) => setWord(wordFromHome)}></Home>}></Route>
+        </Routes>
+      </BrowserRouter>
+      <footer>
+        Made with ❤ by <a href="https://female-coders.at/">Female Coders Linz</a>
+      </footer>
     </div>
   );
 }
---
+++ src/Home.tsx
@@ -0,0 +1,51 @@
+import './Home.css';
+import { Button, TextField } from "@mui/material";
+import React, { useState } from "react";
+import { useNavigate } from "react-router";
+import { ChangeEvent } from 'react';
+
+interface HomeProps {
+    checkWord: (word: string) => void
+}
+
+export default function Home(props: HomeProps) {
+
+    let [enteredWord, setEnteredWord] = useState<string>("");
+    let navigate = useNavigate();
+
+    const navigateToResults = () => {
+        props.checkWord(enteredWord);
+        navigate("/result");
+    }
+
+    const updateEnteredWord = (event: ChangeEvent<HTMLInputElement>) => {
+        setEnteredWord(event.target.value)
+    }
+
+    return (
+        <>
+            <section>
+                <h1>What is good bad words?</h1>
+
+                <p>Swearing is normal and can even be benefitial for your health.
+                    Many of us might have common swear words in our daily language but we should stop to ask ourselves - Is this still okay?
+                    Good bad words is meant to be able to tell you if your favorite swear words could be inappropriate, and even better, to suggest you a better one to express your feelings 😉.
+                </p>
+
+                <h2>Do I really need this?</h2>
+
+                <p>Maybe not but hey, what's wrong with checking if your favorite swear word could be hurt- or disrespectful to any group of people?</p>
+            </section>
+            <section className="flex-column">
+                <h2>Enter your swear word here:</h2>
+                <TextField
+                    onChange={updateEnteredWord}
+                    value={enteredWord}
+                    label="Is this still okay?"
+                    variant="outlined">
+                </TextField>
+                <Button variant="contained" onClick={navigateToResults}>Check</Button>
+            </section>
+        </>
+    );
+}
